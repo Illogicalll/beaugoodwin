54165a57c17224535043d1722cf29aff9ea154db
Will Murphy <illogicallledits@gmail.com>
2024-06-14 00:27:25 +0100

    theme fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,15 @@
 	import { blur } from 'svelte/transition';
 	export let data;
 	import { Toaster } from '$lib/components/ui/sonner';
+	import { onMount } from 'svelte';
+	import { darkMode } from '$lib/ThemeSwitch.svelte';
+
+	onMount(() => {
+		if (localStorage.getItem('theme') === 'light') {
+			document.documentElement.classList.remove('dark');
+			darkMode.set(false);
+		}
+  });
 </script>
 
 {#key data.url}
