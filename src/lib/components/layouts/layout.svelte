b816d97bd0a86900119ecce34e7cb431ad2cf0df
Will Murphy <illogicallledits@gmail.com>
2024-07-08 20:01:44 +0100

    markdown layout changes

---
+++ src/lib/components/layouts/layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import './github-markdown.css';
+</script>
+
+<main class="markdown-body">
+	<slot />
+</main>
--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,18 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const dirname = path.resolve(fileURLToPath(import.meta.url), '../');
 
 /** @type {import('mdsvex').MdsvexOptions} */
 const mdsvexOptions = {
-	extensions: ['.md']
-}
+	extensions: ['.md'],
+	layout: {
+		default: path.join(dirname, '/src/lib/components/layouts/layout.svelte')
+	}
+};
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
