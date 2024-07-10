1d79edb17e67534faf9c1c03d2c44e27b16da75b
Will Murphy <illogicallledits@gmail.com>
2024-07-10 18:03:34 +0100

    fixed album bug

--- src/routes/blog/album/+page.svelte
+++ src/routes/blog/album/+page.svelte
@@ -3,29 +3,29 @@
 	import ThemeSwitch, { darkMode } from '$lib/ThemeSwitch.svelte';
 	import PageToggle from '$lib/components/ui/page-toggle/PageToggle.svelte';
 	import * as Avatar from '$lib/components/ui/avatar/index.js';
-  import * as Tooltip from '$lib/components/ui/tooltip';
+	import * as Tooltip from '$lib/components/ui/tooltip';
 	import { onMount } from 'svelte';
 	import { register } from 'swiper/element/bundle';
 	import { hasRefreshed } from '$lib/stores';
-	
+
 	register();
 	export let data;
-  let posts = data.posts;
-  posts = posts.filter(post => post.categories?.includes('Album'));
+	let posts = data.posts;
+	posts = posts.filter((post) => post.categories?.includes('Album'));
 
 	let isDesktop = true;
 	const handleResize = () => {
-    isDesktop = window.innerWidth > 900;
-  };
+		isDesktop = window.innerWidth > 900;
+	};
 
 	onMount(() => {
 		handleResize();
-    window.addEventListener('resize', handleResize);
+		window.addEventListener('resize', handleResize);
 		ensureCSS();
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  });
+		return () => {
+			window.removeEventListener('resize', handleResize);
+		};
+	});
 
 	function ensureCSS() {
 		if (!$hasRefreshed) {
@@ -35,16 +35,15 @@
 	}
 
 	function goHome() {
-    goto("/");
-  }
-
+		goto('/');
+	}
 </script>
 
 <div class="absolute top-4 left-4 z-50">
 	<Tooltip.Root openDelay={50}>
 		<Tooltip.Trigger>
 			<button on:click={goHome}>
-				<Avatar.Root style={"width: 40px; height: 40px;"}>
+				<Avatar.Root style={'width: 40px; height: 40px;'}>
 					<Avatar.Image
 						src="https://pbs.twimg.com/profile_images/1693199305325101056/qQbq0OQN_400x400.jpg"
 					/>
@@ -59,11 +58,21 @@
 <ThemeSwitch />
 
 <div class="flex justify-center items-center h-svh w-svw">
-	<swiper-container effect="coverflow" direction={isDesktop ? "horizontal" : "vertical"} coverflow-effect-rotate="50" coverflow-effect-stretch="0" coverflow-effect-depth="100" coverflow-effect-modifier="1" slides-per-view="3" centered-slides="true" navigation={isDesktop ? "true" : "false"}>
+	<swiper-container
+		effect="coverflow"
+		direction={isDesktop ? 'horizontal' : 'vertical'}
+		coverflow-effect-rotate="50"
+		coverflow-effect-stretch="0"
+		coverflow-effect-depth="100"
+		coverflow-effect-modifier="1"
+		slides-per-view={posts.length}
+		centered-slides="true"
+		navigation={isDesktop ? 'true' : 'false'}
+	>
 		{#each posts as post}
 			<!-- svelte-ignore a11y-click-events-have-key-events -->
 			<swiper-slide lazy="true" role="button" tabindex="0" on:click={goto(post.slug)}>
-				<img src={post.img} alt={post.title} loading="lazy" class="cursor-pointer"/>
+				<img src={post.img} alt={post.title} loading="lazy" class="cursor-pointer" />
 			</swiper-slide>
 		{/each}
 	</swiper-container>
@@ -72,3 +81,4 @@
 <div class="absolute bottom-6 left-0 right-0 flex justify-center">
 	<PageToggle />
 </div>
+
